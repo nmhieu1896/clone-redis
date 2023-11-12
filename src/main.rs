ba59daecfe3d756bea9b71db8fe24cfbda467c87
Hieu Nguyen <macbookM1@MacBook-Pro.local>
2023-11-12 15:38:46 +0700

    double PING redis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use tokio::{io::AsyncWriteExt, net::TcpListener, net::TcpStream};
+// use std::{io::Write, net::TcpListener};
+use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpListener, net::TcpStream};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -8,13 +9,26 @@ async fn main() -> anyhow::Result<()> {
 
     loop {
         let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            process(socket).await;
-        });
+
+        // tokio::spawn(async move {
+        process(socket).await;
+        // });
     }
 }
 
 async fn process(mut client_socket: TcpStream) {
-    let (mut ___, mut writer) = client_socket.split();
-    let _ = writer.write(b"+PONG\r\n").await;
+    let (mut reader, mut writer) = client_socket.split();
+    let mut buf = [0; 1024];
+
+    loop {
+        match reader.read(&mut buf).await {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    break;
+                }
+                writer.write_all(b"+PONG\r\n").await.unwrap();
+            }
+            Err(e) => println!("Error: {:?}", e),
+        };
+    }
 }
